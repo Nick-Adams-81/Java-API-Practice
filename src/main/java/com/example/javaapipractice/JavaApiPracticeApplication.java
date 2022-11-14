39f13dc5caac458caf55824c5cc1d739da82eec5
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-14 16:48:42 -0600

    init comit

---
+++ src/main/java/com/example/javaapipractice/JavaApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApiPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaapipractice/JavaApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaapipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
